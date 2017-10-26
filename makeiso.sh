b10c9c2c4e7d35d6a79bb73953915cf98a2caf4a
ludovitsiket <siket.ludovit@gmail.com>
2017-10-26 10:58:29 +0200

    Funkcia iso, kontrola existencie parametru.
    Funkcia syntax.

--- makeiso.sh
+++ makeiso.sh
@@ -3,7 +3,22 @@
 # Script make iso from file or folder (parameter ARG). Require genisoimage utility.
 # 25.10.2017
 
-ARG=$1
-EXT='.iso'
-fname="${ARG%.*}"
-genisoimage -o $fname$EXT $ARG
+function iso {
+    ARG=$1
+    EXT='.iso'
+    fname="${ARG%.*}"
+    genisoimage -o $fname$EXT $ARG
+}
+
+function syntax {
+    echo "Syntax: bash makeiso.sh [argument]"
+    echo "        argument is .zip file or folder (without \ at end of folder name)"
+}
+
+if [ $# -eq 0 ]
+  then
+    syntax
+  else
+    iso $1
+fi
+
